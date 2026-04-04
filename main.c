1ee6b424213c4563813d11128a3813186bb3f2ea
hayeona1109 <hayeona1109@mju.ac.kr>
2026-04-05 01:52:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 /* 
   파일이름: 
-  작 성 자:
-  하 는 일: 
+  작 성 자: 남하연, 26-04-05
+  하 는 일: 스마트 급여 계신 및 근무 스케줄 관리 시스템 
 */
 
 
 #include <stdio.h>
+#include<stdilb.h>
 int main()
-
 {
+system("chcp 65001");
+
 printf("hellp world");
   return0;
 }
