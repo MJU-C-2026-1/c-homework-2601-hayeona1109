4243fe9270ae5bb928ea52bb15d358cd03993230
hayeona1109 <hayeona1109@mju.ac.kr>
2026-04-05 15:02:57 +0900

    Fix variable declarations and syntax errors in main.c

--- main.c
+++ main.c
@@ -6,28 +6,28 @@
 
 
 #include <stdio.h>
-#include<stdilb.h>
+#include<stdlib.h>
 int main()
 {
 system("chcp 65001");
 //1. 변수 선언 (5개 이상, 3가지 자료형 사용)
-chat brand_initial; // 알바 장소 이니셜 (chat)
-int houly_wage;  // 기본 시급(int)
+char brand_initial; // 알바 장소 이니셜 (chat)
+int hourly_wage;  // 기본 시급(int)
 int working_hours; // 일일 근무 시간(int)
 float meal_allowance; // 식대 지원비(float)
-float total_day_pay: // 최종 예상 알급(계산결과)
+float total_day_pay; // 최종 예상 알급(계산결과)
 
 //2. 프로그램 시작 화면 출력
 printf("========================================\n");
 printf("스마트 알바 급여 관리 시스템\n");
 printf("=========================================\n\n");
 
-//3. 사용자 입력 받기(scanf)
+//3. 사용자 입력 x받기(scanf)
  printf( "알바 장소의 영문 이니셜을 입력하세요:");
  scanf(" %c", &brand_initial); // %c앞 공백 필수...
 
   printf("현재 본인의 시급(정수)를 입력하세요:");
-  scanf("%d", &working_hours);
+  scanf("%d", &hourly_wage);
 
   printf("오늘 근무한 시간(정수)을 입력하세요:");
   scanf("%d", &working_hours);
@@ -47,7 +47,7 @@ printf("=========================================\n\n");
   printf("오늘도 고생 많으셨습니다!");
   printf("====================================\n");
   
-  return0;
+  return 0;
 }
 
 
